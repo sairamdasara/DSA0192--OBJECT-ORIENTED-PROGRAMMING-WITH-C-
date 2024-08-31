f9a01dff30103008dfd8ef06ecd40abd3525e49f
sairamdasara <126001270+sairamdasara@users.noreply.github.com>
2024-08-31 10:13:10 +0530

    Add files via upload

---
+++ 28.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int num,sum=0,rem;
+	cout <<"enter a number";
+	cin >>num;
+	
+	while (num!=0)
+	{
+		rem=num%10;
+		sum=sum+rem;
+		num=num/10;
+	}
+	cout <<"sum of elements in num ="<<sum;
+}
